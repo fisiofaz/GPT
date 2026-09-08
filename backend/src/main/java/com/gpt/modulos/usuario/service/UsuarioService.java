72bf93584e9beabb824153d2078531f949d43c1c
Fábio André Zatta <fisiofaz8@gmail.com>
2026-09-08 13:40:24 -0300

    fix: impede auto-inativacao de usuario

--- backend/src/main/java/com/gpt/modulos/usuario/service/UsuarioService.java
+++ backend/src/main/java/com/gpt/modulos/usuario/service/UsuarioService.java
@@ -222,6 +222,12 @@ public class UsuarioService {
     	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         Usuario usuarioAutenticado = (Usuario) authentication.getPrincipal();
+        
+        if (usuarioAutenticado.getId().equals(usuario.getId())) {
+            throw new IllegalArgumentException(
+                    "Você não pode inativar o próprio usuário."
+            );
+        }
 
         boolean isAdminGeral = usuarioAutenticado.getRoles().stream()
                 .anyMatch(role -> "ROLE_ADMIN_GERAL".equals(role.getNome()));
