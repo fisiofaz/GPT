b4d445ac45a8c51a8a0dee2c1092524c52ea58a3
Fábio André Zatta <fisiofaz8@gmail.com>
2026-09-08 14:05:43 -0300

    feat: transfere role de superintendente na criacao

--- backend/src/main/java/com/gpt/modulos/usuario/service/UsuarioService.java
+++ backend/src/main/java/com/gpt/modulos/usuario/service/UsuarioService.java
@@ -97,10 +97,25 @@ public class UsuarioService {
         }
 
         usuario.setRoles(roles);
-
+        
         Usuario salvo = usuarioRepository.save(usuario);
 
+        boolean isSuperintendente = usuarioAutenticado.getRoles().stream()
+                .anyMatch(role -> "ROLE_SUPERINTENDENTE_SERVICO".equals(role.getNome()));
+
+        boolean novoUsuarioEhSuperintendente =
+                dto.getRoles().contains("ROLE_SUPERINTENDENTE_SERVICO");
+
+        if (isSuperintendente && novoUsuarioEhSuperintendente) {
+            usuarioAutenticado.getRoles().removeIf(
+                    role -> "ROLE_SUPERINTENDENTE_SERVICO".equals(role.getNome())
+            );
+
+            usuarioRepository.save(usuarioAutenticado);
+        }
+
         return toDTO(salvo);
+        
     }
     
     @Transactional
